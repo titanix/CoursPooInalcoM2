f076adce60efd4fcbc142982b3b6013281da60f8
Louis Lecailliez <louis.Lecailliez@outlook.fr>
2024-10-20 17:17:15 +0200

    Ajout du projet de la Seance5.

---
+++ Seance5/Program.cs
@@ -0,0 +1,49 @@
+﻿Rectangle rectangle = new Rectangle(2, 4);
+Console.WriteLine(rectangle.Surface);
+
+// on ne peut pas modifier la surface !
+//rectangle.Surface = 5; // erreur de compilation
+
+Console.WriteLine(rectangle.Longueur);
+rectangle.Longueur = 10;
+
+struct Rectangle
+{
+    int longueur;
+
+    // propriété avec back-field (longueur juste au-dessus)
+    public int Longueur
+    {
+        get
+        {
+            Console.WriteLine("get de Longueur appelé");
+
+            return longueur;
+        }
+
+        set
+        {
+            Console.WriteLine("set de Longueur appelé");
+
+            longueur = value;
+        }
+    }
+
+    // propriété auto-implémentée, en lecture seule
+    public int Largeur { get; private set; }
+
+    // propriété calculée
+    public int Surface
+    {
+        get
+        {
+            return Longueur * Largeur;
+        }
+    }
+
+    public Rectangle(int longueur, int largeur)
+    {
+        Longueur = longueur;
+        Largeur = largeur;
+    }
+}
