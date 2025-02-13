f86073337f5c718cc1249e1ee0b2ed512a37655f
Shen <shenyuntian0@gmail.com>
2025-02-13 14:35:01 +0100

    Add other tests

--- PokedexS2/Tests/UnitTest1.cs
+++ PokedexS2/Tests/UnitTest1.cs
@@ -18,4 +18,48 @@ public class UnitTest1
         Assert.Equal(1, pokedex.Count);
     }
 
+    [Fact]
+    public void DiscoverMonsterTest()
+    {
+        // Arrange
+        Pokemon pokemon = new Pokemon(25, "Picachu", Type.Electric);
+        Pokedex.Pokedex pokedex = new Pokedex.Pokedex();
+        pokedex.Add(pokemon);
+
+        // Act
+        pokedex.Discover(pokemon);
+
+        // Assert
+        Assert.True(pokemon.Discovered);
+    }
+
+    [Fact]
+    public void GetMonsterByIdTest()
+    {
+        // Arrange
+        Pokemon pokemon = new Pokemon(25, "Picachu", Type.Electric);
+        Pokedex.Pokedex pokedex = new Pokedex.Pokedex();
+        pokedex.Add(pokemon);
+
+        // Action
+        Pokemon result = pokedex.Get(25);
+
+        // Assert
+        Assert.Equal(pokemon, result);
+    }
+
+    [Fact]
+    public void GetMonsterByNameTest()
+    {
+        // Arrange
+        Pokemon pokemon = new Pokemon(25, "Picachu", Type.Electric);
+        Pokedex.Pokedex pokedex = new Pokedex.Pokedex();
+        pokedex.Add(pokemon);
+
+        // Action
+        Pokemon result = pokedex.Get("Picachu");
+
+        // Assert
+        Assert.Equal(pokemon, result);
+    }
 }
